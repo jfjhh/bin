4cba0c33bbf8037f0ea6d4fbcf2c1a1faa6d7e5d
jfjhh <alex.striff1@gmail.com>
2015-07-02 00:00:22 -0700

    auto push_repos (07-02-15/00:00:22).

--- clean_vimundo.sh
+++ clean_vimundo.sh
@@ -1,18 +1,52 @@
 #!/bin/bash
 #
+# Extraneous vim ghost file cleaner.
+# Alex Striff
+#
 # Removes all extraneous vimundo files from users ~/.vimundo directory.
-# TODO: Also prune off files from :mkview if they exist.
+# Also prunes off files from :mkview if they exist (in ~/.vim/view).
 #
 
-DELETED=0
+REAL_MKVIEW=""
+REAL_VIMUNDO=""
+DELETED_VIEW=0
+DELETED_VIMUNDO=0
 
-# Vimundo stores paths as "%home%user%foo.txt", so convert '%' to '/'.
-for i in `ls "$HOME/.vimundo" | sed 's/\%/\//g'`; do
-	if [ ! -f "${i}" ]; then
-		FILE="`echo ${i} | sed 's/\//\%/g'`"
-		rm -v "$HOME/.vimundo/$FILE" && let DELETED++
-	fi
-done
+# :mkview stores paths as "~=+home=+user=+foo.txt", so convert to path.
+function mkview_to_path()
+{
+	REAL_MKVIEW="`echo \"$1\" \
+		| sed -e 's/=//g' -e 's/+/\//g' -e \"s%\~%${HOME}%g\"`"
+}
 
-echo "$DELETED extraneous vimundo files deleted."
+# .vimundo stores paths as "%home%user%foo.txt", so convert to path.
+function vimundo_to_path()
+{
+	REAL_VIMUNDO="`echo \"$1\" | sed 's/\%/\//g'`"
+}
+
+# :mkview files.
+if [ -d "$HOME/.vim/view" ]; then
+	FILES="`ls $HOME/.vim/view`"
+	for i in $FILES; do
+		mkview_to_path $i
+		if [ ! -f "${REAL_MKVIEW}" ]; then
+			rm -v "${i}" && let DELETED_VIEW++
+		fi
+	done
+fi
+
+# Vimundo files.
+if [ -d "$HOME/.vimundo" ]; then
+	FILES="`ls $HOME/.vimundo`"
+	for i in $FILES; do
+		vimundo_to_path $i
+		if [ ! -f "${REAL_VIMUNDO}" ]; then
+			FILE="`echo ${i} | sed 's/\//\%/g'`"
+			rm -v "${i}" && let DELETED_VIMUNDO++
+		fi
+	done
+fi
+
+echo "$DELETED_VIMUNDO vimundo and $DELETED_VIEW :mkview ghost files deleted."
 
